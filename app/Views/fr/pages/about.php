81acbb596f53c376a74496ae3d058d6e3bd5912b
muhamad-bari <barsid589@gmail.com>
2023-12-26 07:01:24 +0700

    faq include

--- app/Views/fr/pages/about.php
+++ app/Views/fr/pages/about.php
@@ -32,7 +32,7 @@
               <p class="text mb25">PT. Management Mandiri Internasional is one of the licensed Indonesian labor placement companies for overseas markets. As an overseas placement specialist, we focus on understanding our client's needs, offering consultation, and providing workers that match the client's demands.
               <br class="d-none d-md-block"> Now, requirements and regulations pertaining to human resources are constantly changing. Many companies strive to obtain labor supply services and workers that are suitable for their company's needs, however, finding the right solution may be a challenge.
               </p>
-              <a href="<?= base_url('talent') ?>" class="ud-btn btn-thm-border">Find Talent<i class="fal fa-arrow-right-long"></i></a>
+              <a href="<?= base_url('login') ?>" class="ud-btn btn-thm-border">Find Talent<i class="fal fa-arrow-right-long"></i></a>
             </div>
           </div>
         </div>
@@ -98,25 +98,23 @@
               <div class="col-sm-6">
                 <div class="funfact-style1 bdrs16 text-center ms-md-auto">
                   <ul class="ps-0 mb-0 d-flex justify-content-center">
-                    <li><div class="timer title mb15">4</div></li>
-                    <li><span>.9/5</span></li>
+                    <li><span>ISO Certified</span></li>
                   </ul>
-                  <p class="fz15 dark-color">Clients rate <br>professionals on Management Mandiri Internasional</p>
+                  <p class="fz15 dark-color">Internationally Registered and Certified with ISO 9001:2015, ISO 45001:2018, ISO, 14001:2015, and ISO 37001:2016</p>
                 </div>
                 <div class="funfact-style1 bdrs16 text-center ms-md-auto">
                   <ul class="ps-0 mb-0 d-flex justify-content-center">
-                    <li><div class="timer title mb15">96</div></li>
-                    <li><span>%</span></li>
+                    <li><span>Official</span></li>
                   </ul>
-                  <p class="fz15 dark-color">95% of customers are satisfied through to see their <br>freelancers</p>
+                  <p class="fz15 dark-color">Has been officially recognized and certified by Indonesian Migrant Workers Protection Agency</p>
                 </div>
               </div>
               <div class="col-sm-6">
                 <div class="funfact-style1 bdrs16 text-center">
                   <ul class="ps-0 mb-0 d-flex justify-content-center">
-                    <li><div class="title mb15">Award</div></li>
+                    <li><div class="title mb15">Accredited</div></li>
                   </ul>
-                  <p class="fz15 dark-color">G2’s 2021 Best <br>Software Awards</p>
+                  <p class="fz15 dark-color">Accredited by National Accreditation Committee with SNI ISO/IEC 17021-1:2015 and SNI ISO/IEC 17021-3:2017</p>
                 </div>
               </div>
             </div>
@@ -235,67 +233,5 @@
       </div>
     </section>
 
-    <!-- Faq Area -->
-    <section class="our-faq pb90">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 m-auto wow fadeInUp" data-wow-delay="300ms">
-            <div class="main-title text-center">
-              <h2 class="title">Frequently Asked Questions</h2>
-              <p class="paragraph mt10">Lorem ipsum dolor sit amet, consectetur.</p>
-            </div>
-          </div>
-        </div>
-        <div class="row wow fadeInUp" data-wow-delay="300ms">
-          <div class="col-xl-8 mx-auto">
-            <div class="ui-content">
-              <div class="accordion-style1 faq-page mb-4 mb-lg-5">
-                <div class="accordion" id="accordionExample">
-                  <div class="accordion-item active">
-                    <h2 class="accordion-header" id="headingOne">
-                      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">What methods of payments are supported?</button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                      <div class="accordion-body">Cras vitae ac nunc orci. Purus amet tortor non at phasellus ultricies hendrerit. Eget a, sit morbi nunc sit id massa. Metus, scelerisque volutpat nec sit vel donec. Sagittis, id volutpat erat vel.</div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingTwo">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Can I cancel at anytime?</button>
-                    </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                      <div class="accordion-body">Cras vitae ac nunc orci. Purus amet tortor non at phasellus ultricies hendrerit. Eget a, sit morbi nunc sit id massa. Metus, scelerisque volutpat nec sit vel donec. Sagittis, id volutpat erat vel.</div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingThree">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">How do I get a receipt for my purchase?</button>
-                    </h2>
-                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                      <div class="accordion-body">Cras vitae ac nunc orci. Purus amet tortor non at phasellus ultricies hendrerit. Eget a, sit morbi nunc sit id massa. Metus, scelerisque volutpat nec sit vel donec. Sagittis, id volutpat erat vel.</div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingFour">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Which license do I need?</button>
-                    </h2>
-                    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-                      <div class="accordion-body">Cras vitae ac nunc orci. Purus amet tortor non at phasellus ultricies hendrerit. Eget a, sit morbi nunc sit id massa. Metus, scelerisque volutpat nec sit vel donec. Sagittis, id volutpat erat vel.</div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingFive">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">How do I get access to a theme I purchased?</button>
-                    </h2>
-                    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
-                      <div class="accordion-body">Cras vitae ac nunc orci. Purus amet tortor non at phasellus ultricies hendrerit. Eget a, sit morbi nunc sit id massa. Metus, scelerisque volutpat nec sit vel donec. Sagittis, id volutpat erat vel.</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+<?= $this->include('fr/layout/faq') ?>
 <?= $this->endSection () ?>
