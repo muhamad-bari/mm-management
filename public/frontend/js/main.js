73774314246ecff2a0bdab2aff7c25bcd3227c9f
muhamad-bari <barsid589@gmail.com>
2023-12-29 07:57:21 +0700

    bahasa fix

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,9 +1,10 @@
-$('.language').on('change', function() {
+$('.language').on('change', function () {
     var lang = $(this).val();
     var currentUrl = window.location.href;
     console.log(currentUrl)
     var baseURL = currentUrl.split('/').slice(0, 3).join('/');
 
-var page = currentUrl.split('/')
-window.location.href = baseURL + '/' + lang +'/'+ page[4];
+    var page = currentUrl.split('/')
+    var resUrl = page.slice(4, page.length).join('/');
+    window.location.href = baseURL + '/' + lang + '/' + resUrl;
 });
