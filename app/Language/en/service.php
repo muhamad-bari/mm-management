12d48429fc63f48627e86aec891f5912ce912dbd
muhamad-bari <barsid589@gmail.com>
2024-01-04 08:37:40 +0700

    bug fix di dashboard

---
+++ app/Language/en/service.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'home' => 'Home',
+
+    'subservice' => 'Provide the Best Experience with Our Services',
+    'vid' => 'Profile Video PT. Management Mandiri Internasional',
+
+    'sort' => 'Sort By',
+    'best' => 'Best Seller',
+    'recommended' => 'Recommended',
+    'new' => 'Newest',
+
+    'starting' => 'Starting at',
+    'service' => 'Service',
+    'house' => 'Household Assistant',
+    'baby' => 'Baby Sitter',
+    'elder' => 'Elderly Nurse',
+    'driver' => 'Driver',
+    'chef' => 'Chef',
+    'Barista' => 'Barista',
+    'Waitress' => 'Waitress',
+    'Tailor' => 'Tailor',
+    'Beautycian' => 'Beautycian',
+    'reviews' => 'Reviews',
+];
