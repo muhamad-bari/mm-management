b0a854e29f3d219ef487cf24cda526fac203179a
muhamad-bari <barsid589@gmail.com>
2024-01-12 10:36:10 +0700

    ok

--- app/Views/fr/pages/gallery.php
+++ app/Views/fr/pages/gallery.php
@@ -31,150 +31,151 @@
 		</div>
 	</section>
 	<!-- Our Agents -->
-	<div class="container-fluid portfolio-section portfolio-4-col">
-	<div class="kategori-container p-4">
-                <!-- <h4>Kategori</h4> -->
-                <div class="bordered kategori">
-                </div>
-            </div>
-		<!-- Portfolio List -->
-		<div class="row portfolio-list galeri-fr">
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box" data-ktr='berangkat'>
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-1.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Pelatihan Lanayan Baby Sitter</a>
-						<a href="#" data-img="<?= base_url() ?>frontend/images/gallery/home-portfolio-1.jpg" data-bs-toggle="modal" data-bs-target="#galeri-modal" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
+	<!-- <div class="container-fluid portfolio-section portfolio-4-col">
+		<div class="kategori-container p-4">
+			<h4>Kategori</h4>
+			<div class="bordered kategori">
 			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box" data-ktr='pelatihan'>
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-2.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Persiapan Keberangkatan</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-2.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
+		</div> -->
+	<!-- Portfolio List -->
+	<!-- <div class="row portfolio-list galeri-fr"> -->
+	<div class="row portfolio-list">
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box" data-ktr='berangkat'>
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-1.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Pelatihan Lanayan Baby Sitter</a>
+					<a href="#" data-img="<?= base_url() ?>frontend/images/gallery/home-portfolio-1.jpg" data-bs-toggle="modal" data-bs-target="#galeri-modal" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
 				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-3.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Persiapan Keberangkatan</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-3.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-4.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Persiapan Keberangkatan</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-4.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-5.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Persiapan Keberangkatan</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-5.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-6.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Persiapan Keberangkatan</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-6.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-7.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Pelatihan Materi</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-7.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-8.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Persiapan Keberangkatan</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-8.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-9.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Pelatihan Layanan Chef</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-9.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-10.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Pelatihan Materi</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-10.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-11.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Pelatihan Layanan Chef</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-11.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
-				<div class="portfolio-detail">
-					<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-12.jpg" alt="Gallery" /></i>
-					<div class="portfolio-content">
-						<a href="#">Pelatihan Layanan Asisten Rumah Tangga</a>
-						<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-12.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
-					</div>
-				</div>
-			</div>
-		</div><!-- Row -->
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="mbp_pagination mt30 text-center">
-				<ul class="page_navigation pageButtons">
-					<li class="page-item">
-						<a class="page-link" href="#"> <span class="fas fa-angle-left"></span></a>
-					</li>
-					<li class="page-item active" aria-current="page">
-						<a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="page-item"><a class="page-link" href="#">2</a></li>
-					<li class="page-item"><a class="page-link" href="#">3</a></li>
-					<li class="page-item"><a class="page-link" href="#">4</a></li>
-					<li class="page-item d-inline-block d-sm-none"><a class="page-link" href="#">...</a></li>
-					<li class="page-item"><a class="page-link" href="#">5</a></li>
-					<li class="page-item d-none d-sm-inline-block"><a class="page-link" href="#">...</a></li>
-					<li class="page-item d-none d-sm-inline-block"><a class="page-link" href="#">20</a></li>
-					<li class="page-item">
-						<a class="page-link" href="#"><span class="fas fa-angle-right"></span></a>
-					</li>
-				</ul>
-				<!-- <p class="mt10 mb-0 pagination_page_count text-center">1 – 20 of 300+ Talent available</p> -->
-				<br>
 			</div>
 		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box" data-ktr='pelatihan'>
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-2.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Persiapan Keberangkatan</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-2.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-3.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Persiapan Keberangkatan</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-3.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-4.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Persiapan Keberangkatan</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-4.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-5.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Persiapan Keberangkatan</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-5.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-6.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Persiapan Keberangkatan</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-6.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-7.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Pelatihan Materi</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-7.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-8.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Persiapan Keberangkatan</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-8.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-9.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Pelatihan Layanan Chef</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-9.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-10.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Pelatihan Materi</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-10.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-11.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Pelatihan Layanan Chef</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-11.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 portfolio-box">
+			<div class="portfolio-detail">
+				<i><img src="<?= base_url('') ?>frontend/images/gallery/home-portfolio-12.jpg" alt="Gallery" /></i>
+				<div class="portfolio-content">
+					<a href="#">Pelatihan Layanan Asisten Rumah Tangga</a>
+					<a href="<?= base_url('') ?>frontend/images/gallery/home-portfolio-12.jpg" class="zoom" title="Work Title Goes right here"><i class="fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
+	</div><!-- Row -->
+</div>
+<div class="container">
+	<div class="row">
+		<div class="mbp_pagination mt30 text-center">
+			<ul class="page_navigation pageButtons">
+				<li class="page-item">
+					<a class="page-link" href="#"> <span class="fas fa-angle-left"></span></a>
+				</li>
+				<li class="page-item active" aria-current="page">
+					<a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="page-item"><a class="page-link" href="#">2</a></li>
+				<li class="page-item"><a class="page-link" href="#">3</a></li>
+				<li class="page-item"><a class="page-link" href="#">4</a></li>
+				<li class="page-item d-inline-block d-sm-none"><a class="page-link" href="#">...</a></li>
+				<li class="page-item"><a class="page-link" href="#">5</a></li>
+				<li class="page-item d-none d-sm-inline-block"><a class="page-link" href="#">...</a></li>
+				<li class="page-item d-none d-sm-inline-block"><a class="page-link" href="#">20</a></li>
+				<li class="page-item">
+					<a class="page-link" href="#"><span class="fas fa-angle-right"></span></a>
+				</li>
+			</ul>
+			<p class="mt10 mb-0 pagination_page_count text-center">1 – 20 of 300+ Talent available</p>
+			<br>
+		</div>
 	</div>
+</div>
 
 
-	<?= $this->endSection() ?>
+<?= $this->endSection() ?>
